2b54afd653df326fc779cadce821a7b1c41bca43
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-16 23:52:14 +0400

    Максимальное из трех чисел

---
+++ Seminar_1/HomeWork_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int a3 = Convert.ToInt32(Console.ReadLine());
+int max=a1;
+
+if (max<a2) max=a2;
+{
+    if (max<a3) max=a3;
+}
+Console.WriteLine($"Максимальное число: {max}");
