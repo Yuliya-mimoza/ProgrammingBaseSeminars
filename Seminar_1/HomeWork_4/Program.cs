209fb7e8b6c3cbfd1b3f41be740deb7109b00fa6
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-17 00:39:53 +0400

    Четные числа диапазона

---
+++ Seminar_1/HomeWork_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите целое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа диапазона: " );
+int i = 1;
+
+while (i <= n)
+{
+    if (i % 2 == 0) Console.WriteLine(i);
+    i = i + 1;
+}
