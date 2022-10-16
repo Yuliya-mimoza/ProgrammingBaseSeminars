2713014962c6159af5bd270aa8196709a302e167
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-16 18:06:36 +0400

    Нахождение квадрата числа

---
+++ Seminar_1/ConsoleApp_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает целое число и выдает его квадрат
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+int sqrnumber = number * number;
+Console.Write($"Квадрат числа = {sqrnumber}");
