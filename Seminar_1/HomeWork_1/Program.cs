621a279dc5b4c80cd3dc31ae261ac1240b0a8f22
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-16 23:27:28 +0400

    Большее или меньшее из двух чисел

---
+++ Seminar_1/HomeWork_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+
+if (a1 > a2)
+{
+    Console.WriteLine($"Первое число: {a1} больше второго: {a2}");
+}
+else
+{
+    Console.WriteLine($"Второе число: {a2} больше первого: {a1}");
+}
