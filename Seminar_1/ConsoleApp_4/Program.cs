21b405067ee0ed26ca22d51bd934c23d746acfb7
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-16 23:14:17 +0400

    Все целые числа в промежутке от -N до N

---
+++ Seminar_1/ConsoleApp_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// программа, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int negativN = N * -1;
+while (negativN <= N)
+{
+    Console.WriteLine(negativN);
+    negativN = negativN + 1;
+}
