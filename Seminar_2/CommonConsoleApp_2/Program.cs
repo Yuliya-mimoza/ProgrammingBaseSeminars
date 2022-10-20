c5417102bfacaa89582db6780e14305669cc3e22
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-21 00:17:12 +0400

    Наибольшая цифра числа в диапазоне

---
+++ Seminar_2/CommonConsoleApp_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// программа, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int a1 = number / 10;
+int a2 = number % 10;
+
+if (a1 > a2)
+{
+    Console.WriteLine($"Наибольшая цифра в числе: {a1} ");
+}
+
+if (a2 > a1)
+{
+    Console.WriteLine($"Наибольшая цифра в числе: {a2}");
+}
+
+if (a1 == a2)
+{
+    Console.WriteLine($"Цифры в этом числе равны: {a1} и {a2}");
+}
