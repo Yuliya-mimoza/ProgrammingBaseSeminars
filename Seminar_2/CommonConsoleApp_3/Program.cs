798cb9483ee53de2dca76c7e16f338b7932391d3
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-22 01:03:09 +0400

    вывод случайного трёхзначного числа и удаление второй цифры этого числа

---
+++ Seminar_2/CommonConsoleApp_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// программа, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int number =new Random().Next(100,1000);
+Console.WriteLine(number);
+
+int a1=number/100*10;
+int a2=number%10;
+
+int number2=a1+a2;
+Console.WriteLine($"Новое число после удаления второй цифры = {number2}");
---
+++ Seminar_2/CommonConsoleApp_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа, которая принимает на вход два числа и выводит, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % number2 == 0)
+{
+    Console.WriteLine($"Второе число кратно первому числу");
+}
+else
+{
+    Console.WriteLine($"Второе число НЕ кратно первому числу, остаток = {number1 % number2}");
+}
