1af10bc6f0ccb52c7b4b0058cbfef72a6ca0f68a
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-23 00:43:17 +0400

    Вывод третьей цифры заданного числа или сообщаем, что ее нет

---
+++ Seminar_2/HomeWork2_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 999)
+{
+    number = number / 10;
+}
+    
+if (number > 99)
+
+{
+    int n1 = number % 100;
+    int n2 = n1 % 10;
+    Console.WriteLine($"Третья цифра числа = {n2}");
+}
+else if (number < 100)
+{
+    Console.WriteLine($"Не верно введено число! Третья цифра отсутствует! ");
+}
