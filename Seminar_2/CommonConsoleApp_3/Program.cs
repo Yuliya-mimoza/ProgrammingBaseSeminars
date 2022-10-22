514e142ed70f068be2151628eed3dacd6dd702ef
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-22 22:55:11 +0400

    Вывод случайного трёхзначного числа и удаление второй цифры этого числа

--- Seminar_2/CommonConsoleApp_3/Program.cs
+++ Seminar_2/CommonConsoleApp_3/Program.cs
@@ -6,4 +6,4 @@ int a1=number/100*10;
 int a2=number%10;
 
 int number2=a1+a2;
-Console.WriteLine($"Новое число после удаления второй цифры = {number2}");
+Console.WriteLine($"Новое число после удаления второй цифры = {number2}"); 
