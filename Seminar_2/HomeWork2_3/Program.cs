0cd0d5a22fbe845037c8c3e9d87a89e8ad57e7eb
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-22 19:31:54 +0400

    Цифра на входе, обозначающая день недели и проверка на выходной

---
+++ Seminar_2/HomeWork2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите номер дня недели от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 | number > 7)
+{
+    Console.WriteLine("Номер дня недели введен не верно!");
+}
+ else
+ if (number == 6 | number == 7)
+ {
+     Console.WriteLine("Этот день является выходным");
+ }
+else
+{
+    Console.WriteLine("Этот день НЕ является выходным");
+}
