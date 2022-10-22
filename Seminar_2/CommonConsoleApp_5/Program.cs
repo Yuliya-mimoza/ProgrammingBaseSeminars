68f96a3c61320555ff3b0ffeab2c59920135325f
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-23 00:03:57 +0400

    Проверка одновременной кратности числа 7 и 23

---
+++ Seminar_2/CommonConsoleApp_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Число кратно и 7, и 23 ");
+}
+else
+{
+    Console.WriteLine("Двойная кратность не достигнута ");
+}
