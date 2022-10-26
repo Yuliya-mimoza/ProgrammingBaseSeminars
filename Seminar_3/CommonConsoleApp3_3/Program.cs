8da26b2fadd4ac791195eae26716085a3f09febd
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-26 21:26:10 +0400

    Вычисление расстояния между взятыми на плоскости двумя точками

---
+++ Seminar_3/CommonConsoleApp3_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+// Вычисление расстояния между взятыми на плоскости двумя точками
+double rast(int x1, int y1, int x2, int y2)
+{
+    double length1 = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2));
+    return length1;
+}
+
+Console.WriteLine("Введите X первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите X второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double length = rast(x1, y1, x2, y2);
+Console.WriteLine($"Расстояние между взятыми на плоскости двумя точками = {length}");
