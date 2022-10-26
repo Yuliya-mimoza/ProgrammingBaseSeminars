0ef5cba27bf2ae3f1244f108c0d501db827a22a5
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-26 22:01:10 +0400

    Принимает на вход число (N), и выдает таблицу квадратов чисел от 1 до N

---
+++ Seminar_3/CommonConsoleApp3_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа, которая принимает на вход число (N),
+// и выдает таблицу квадратов чисел от 1 до N.
+
+void PrintSquaresToConsole(int n)
+{
+    for (int i=1;i<=n;i++)
+    {
+        Console.WriteLine(i*i);
+    }
+}
+
+PrintSquaresToConsole(10);
