96e055a143a642f2f54fff5defde87cf3e670c4a
Yuliya <m.i.m.o.z.a@mail.ru>
2022-10-27 00:36:37 +0400

    Вводим N, выводит таблицу кубов чисел от 1 до N

---
+++ Seminar_3/HomeWork3_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void PrintSquaresToConsole(int n)
+{
+    for (int i=1;i<=n;i++)
+    {
+        Console.WriteLine(i*i*i);
+    }
+}
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintSquaresToConsole(n);
