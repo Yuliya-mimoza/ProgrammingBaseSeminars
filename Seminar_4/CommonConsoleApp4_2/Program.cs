424e54bc4c37df324a78bbd60fa75dfa7aff9327
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-09 23:49:25 +0400

    Принимает на вход число и выдаёт количество цифр в числе

---
+++ Seminar_4/CommonConsoleApp4_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+string strNumber = Math.Abs(number).ToString();
+Console.WriteLine(strNumber.Length);
