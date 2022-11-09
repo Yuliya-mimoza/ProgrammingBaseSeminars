33b793758de817bb1bd6357dff6259e168eb045f
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-10 00:02:56 +0400

    Принимает на вход число N и выдаёт произведение чисел от 1 до N

---
+++ Seminar_4/CommonConsoleApp4_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// программа, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+int GetPr (int number)
+{
+    int pr=1;
+for (int i=1; i<=number;i++)
+{
+    pr=pr*i;
+}
+return pr;
+}
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int pr=GetPr(number);
+Console.WriteLine($"произведение чисел от 1 до {number} = {pr}");
