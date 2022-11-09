ebdeb85dc0e3cfad229f5a73d6fea85fad16467a
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-09 20:36:15 +0400

    принимает на вход число (А) и выдаёт сумму чисел от 1 до А

---
+++ Seminar_4/CommonConsoleApp4_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// программа, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int GetSum(int number)
+{
+    int sum = 0;
+
+    // используя "WHILE"
+
+    /*int tempValue=1;
+    while (tempValue<=number)
+    {
+        sum=sum+tempValue;
+        tempValue++;
+    }*/
+
+    // используя "FOR"
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    
+    return sum;
+}
+
+Console.WriteLine("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = GetSum(number);
+Console.WriteLine($"Сумма = {sum}");
