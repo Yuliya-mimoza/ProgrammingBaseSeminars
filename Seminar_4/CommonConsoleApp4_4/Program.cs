19524e7e33c479c7e4725c60348d1ae54591906c
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-10 19:52:22 +0400

    выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке

---
+++ Seminar_4/CommonConsoleApp4_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// программа, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array=new int[8];
+Console.WriteLine(array.Length);
+
+for (int i=0; i<array.Length;i++)
+{
+    array[i]=new Random().Next(0,2);
+    Console.Write(array[i]+", ");
+}
