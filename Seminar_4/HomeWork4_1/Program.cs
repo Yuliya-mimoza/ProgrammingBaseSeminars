230b9c4c7fb3c69401502593bccf9478924d1151
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-10 20:42:37 +0400

    программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B

---
+++ Seminar_4/HomeWork4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+int Vozvedenie(int a, int b)
+{
+    int rez=1;
+    for (int i = 1; i <= b; i++)
+    {
+        rez = rez * a;
+    }
+    return rez;
+}
+
+
+Console.WriteLine("Введите число, А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень числа, В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int rez = Vozvedenie(a, b);
+Console.WriteLine($"Число А в степени В = {rez}");
