804977688a847ec1262c88296f71c807a8b6a516
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-11 19:45:43 +0400

    Сумма отрицательных и положительных элементов массива из 12 элементов

---
+++ Seminar_5/CommonConsoleApp5_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+int[] array = new int[12];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write(array[i] +" ");
+}
+int sumPositive = 0;
+int sumNegative = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sumPositive += array[i];
+    }
+    else
+    {
+        sumNegative += array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных чисел равна = {sumPositive}");
+Console.WriteLine($"Сумма отрицательных чисел равна = {sumNegative}");
