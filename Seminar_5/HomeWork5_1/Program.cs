ca180eedf6608a765634285baefcabd13e30411d
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-14 01:11:05 +0400

    Нахождение количества четных чисел в заданном массиве, заполненном случайными положительными 3-значными числами

---
+++ Seminar_5/HomeWork5_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+int Counter(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Сколько элементов будет в массиве? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+Console.WriteLine("Текущий массив: ");
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+int count = Counter(array);
+Console.WriteLine($"Количество чётных чисел в массиве: {count} ");
