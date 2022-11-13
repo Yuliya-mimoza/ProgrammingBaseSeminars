164504f27d711568d390de083d376a2a594b11c6
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-13 23:14:54 +0400

    Нахождение суммы нечетных элементов в заданном массиве

---
+++ Seminar_5/HomeWork5_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int Sum ( int[] array)
+{
+    int sum=0;
+    for (int i=0; i<array.Length;i++)
+    {
+        if (i%2!=0)
+        sum=sum+array[i];
+    }
+    return sum;
+}
+Console.WriteLine("Сколько элементов будет в массиве? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+Console.WriteLine("Текущий массив: ");
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+int rez=Sum(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях = {rez}");
