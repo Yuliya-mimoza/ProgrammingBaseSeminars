aea1a589eb09ccd02030ec1aa13ad327fe5371ed
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-13 21:54:08 +0400

    Задаем массив. Получаем из него новый массив через произведение пар чисел (первый*последний, второй*предпоследний и т.д.)

---
+++ Seminar_5/CommonConsoleApp5_5/Program.cs
@@ -0,0 +1,46 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+
+Console.WriteLine("Сколько элементов будет в массиве? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Текущий массив: ");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
+Console.WriteLine("Новый массив: ");
+if (n % 2 == 0)
+{
+    int[] newArray = new int[n / 2];
+    for (int i = 0; i < (n / 2); i++)
+    {
+        newArray[i] = array[i] * array[n - 1 - i];
+        Console.Write(newArray[i] + ", ");
+    }
+}
+else
+{
+    int[] newArray = new int[n / 2 + 1];
+    for (int i = 0; i < (n / 2 + 1); i++)
+    {
+        newArray[i] = array[i] * array[n - 1 - i];
+        if (i == n / 2)
+        {
+            newArray[i] = array[i];
+        }
+        Console.Write(newArray[i] + ", ");
+    }
+}
