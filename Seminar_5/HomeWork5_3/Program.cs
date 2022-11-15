153eefbabdfe826bc78f8560e5207b38ce06d325
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-15 08:01:48 +0400

    Задаем массив, находим разницу между max и min

---
+++ Seminar_5/HomeWork5_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+// [3 7 22 2 78] -> 76
+
+double Raznica(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    Console.WriteLine($"min = {min}, max = {max}");
+    double rezalt = max - min;
+    return rezalt;
+}
+
+Console.WriteLine("Сколько элементов будет в массиве? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[n];
+Console.WriteLine("Текущий массив: ");
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+double rezalt = Raznica(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {rezalt}");
