9c5a2fbffff788d7887d45d199da7fd157bb3d2e
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-18 23:00:14 +0400

    Программа, которая перевернёт одномерный массив

---
+++ Seminar_6/CommonConsoleApp6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+int [] array=new int[10] {1,2,3,4,5,6,7,8,9,10};
+{
+    for (int i=0; i<array.Length/2; i++)
+    {
+        int temp=array[i];
+        array[i]=array[(array.Length-1)-i];
+        array[(array.Length-1)-i]=temp;
+    }
+}
+for (int i=0; i<array.Length; i++)
+{
+    Console.Write(array[i]+" ");
+}
