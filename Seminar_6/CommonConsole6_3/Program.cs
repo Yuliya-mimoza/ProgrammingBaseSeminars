49c55576123fcda5937d9d49df0bf61fa6a080c1
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-19 12:03:13 +0400

    Преобразование числа из десятичной в двоичную систему

---
+++ Seminar_6/CommonConsole6_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+string Perevod(int chislo)
+{
+    int rez = 0;
+    string str = " ";
+    while (chislo != 0)
+    {
+        rez = chislo % 2;
+        chislo = chislo / 2;
+        str = rez + str;
+    } 
+    return str;
+}
+
+Console.WriteLine("Введите число в десятичной системе: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Данное число будет выглядеть в двоичной системе исчисления вот так: ");
+string str = Perevod(chislo);
+Console.WriteLine($"{str}");
