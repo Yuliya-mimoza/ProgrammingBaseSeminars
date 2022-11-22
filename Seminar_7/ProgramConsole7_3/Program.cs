aaba81e9eef0f45371982f662a4778152f19caec
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-22 20:45:14 +0400

    задать двумерный массив размером m*n, заменить элемент на его квадрат, если оба индекса четные

---
+++ Seminar_7/ProgramConsole7_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+// и замените эти элементы на их квадраты.
+
+// заполнение массива
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                array[i, j] = new Random().Next(1, 10);
+                Console.Write(array[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+// вывод массива с заменой
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i != 0 && i % 2 == 0 && j != 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+                Console.Write(array[i, j] + " ");
+            }
+            else
+            {
+                Console.Write(array[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+FillArray(array);
+Console.WriteLine("Массив с заменой: ");
+PrintArray(array);
