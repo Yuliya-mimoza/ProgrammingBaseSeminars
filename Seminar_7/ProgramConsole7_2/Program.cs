26f94742749b572d7a43803df0a68dcbeedf4038
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-22 17:17:12 +0400

    задать двумерный массив размером m*n, каждый элемент в массиве находится по формуле: A[i,j]= m+n

---
+++ Seminar_7/ProgramConsole7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+//m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void FillArray(int[,]array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = i+j;
+        Console.Write(array[i, j]+ " ");
+    }
+    Console.WriteLine();
+}
+}
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+FillArray(array);
