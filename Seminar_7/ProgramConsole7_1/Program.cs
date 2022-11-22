b22add7d6b01ebb9ab6bfe0d62106d41142887a3
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-22 17:01:44 +0400

    задать двумерный массив размером m*n, заполненный случайными целыми числами

---
+++ Seminar_7/ProgramConsole7_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+// заполнение массива
+void FillArray(int[,]array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 101);
+        Console.Write(array[i, j]+ " ");
+    }
+    Console.WriteLine();
+}
+}
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+FillArray(array);
