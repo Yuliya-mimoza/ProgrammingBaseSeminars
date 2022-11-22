bd03034b8702398df9f1bdf11e6985baa2d5a7bf
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-22 22:51:21 +0400

    задать двумерный массив размером m*n, айдите сумму элементов,

---
+++ Seminar_7/ProgramConsole7_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+// заполнение массива и подсчёт суммы
+void FillArray(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                array[i, j] = new Random().Next(1, 10);
+                Console.Write(array[i, j] + " ");
+                if (i == j)
+                {
+                    sum += array[i, j];
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Сумма элементов, находящихся на главной диагонали = {sum}");
+}
+
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+FillArray(array);
