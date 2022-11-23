732c2177903d1db255b992d892a48b322af9cacc
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-23 19:59:23 +0400

    задать двумерный массив размером m*n, заполненный случайными вещественными числами

---
+++ Seminar_7/HomeWork7_1/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0,10)+Math.Round(new Random().NextDouble(),2);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+FillArray(array);
