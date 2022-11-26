27284e0e59074690b77927ddef4e5a90eac4ff2e
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-26 23:17:50 +0400

    Задача 53.Задать двумерный массив с заменой строк и столбцов

---
+++ Seminar_8/Task55/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                array[i, j] = new Random().Next(1, 10);
+                Console.Write(array[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplacementArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            //array[i, j] = array[j, i];
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количество строк равных количеству столбцов в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите количество столбцов в массиве: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, m];
+Console.WriteLine("Мы получили массив: ");
+FillArray(array);
+Console.WriteLine();
+ReplacementArray(array);
