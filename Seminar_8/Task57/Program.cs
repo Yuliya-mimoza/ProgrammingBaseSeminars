85d3d104603e81cee5ad6f2dc41e060af23f6960
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-28 00:06:43 +0400

    Задача 57. Составить частотный словарь элементов двумерного массива.

---
+++ Seminar_8/Task57/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+int[] array = new int[10] { 1, 1, 4, 5, 2, 2, 5, 4, 2, 4 };
+Dictionary<int, int> dict = new Dictionary<int, int>();
+for (int i = 0; i < array.Length; i++)
+{
+    if (!dict.ContainsKey(array[i]))
+    {
+        dict.Add(array[i], 1);
+    }
+    else
+    {
+        dict[array[i]]++;
+    }
+}
+foreach (var dic in dict)
+{
+    Console.WriteLine($"{dic.Key} встречается {dic.Value} раз");
+}
