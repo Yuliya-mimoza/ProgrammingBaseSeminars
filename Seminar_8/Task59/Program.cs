9f4b120e2a2a6b0fdfa92d1bdef1ed5af333e43e
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-27 15:18:30 +0400

    Задача 59. Задать двумерный массив с заменой строк и столбцов нулями, на пересечении которых расположен наименьший элемент массива

---
+++ Seminar_8/Task59/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, 
+// которая запишет нулями строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим 
+// следующий массив:
+// 0 0 0 0
+// 0 9 4 2
+// 0 2 2 6
+// 0 3 4 7
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                array[i, j] = new Random().Next(1, 101);
+                Console.Write(array[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                Console.Write(array[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+Console.WriteLine("Мы получили массив: ");
+FillArray(array);
+Console.WriteLine();
+int mini = 0;
+int minj = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[mini, minj] > array[i, j])
+        {
+            mini = i;
+            minj = j;
+        }
+    }
+}
+Console.Write(array[mini, minj]);
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    array[i, minj] = 0;
+}
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    array[mini, j] = 0;
+}
+Console.WriteLine();
+PrintArray(array);
