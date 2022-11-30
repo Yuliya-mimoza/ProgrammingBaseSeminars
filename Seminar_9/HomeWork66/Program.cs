cb5f4578da0d330a5d7e2346c2335efb17da6928
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-30 17:53:23 +0400

    Задача 66. Программа, нахождения суммы натуральных элементов в промежутке от M до N.

---
+++ Seminar_9/HomeWork66/Program.cs
@@ -0,0 +1,40 @@
+﻿// // Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SumMN(int m, int n)
+{
+    int sum = 0;
+    sum += m;
+
+    if (m == n)
+    {
+        return sum;
+    }
+    return sum + SumMN(m + 1, n);
+}
+int SumNM(int n, int m)
+{
+    int sum = 0;
+    sum += n;
+    if (m == n)
+    {
+        return sum;
+    }
+    return sum + SumNM(n + 1, m);
+}
+
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m <= n)
+{
+    Console.WriteLine($"Сумма элементов от {m} до {n} равна: {SumMN(m, n)}");
+}
+else
+if (m > n)
+{
+    Console.WriteLine($"Сумма элементов от {m} до {n} равна:{SumNM(n, m)}");
+}
