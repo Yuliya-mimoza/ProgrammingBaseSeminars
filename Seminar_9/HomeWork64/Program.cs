ef0f9719bc627138a91fb560e8eac14705f9564f
Yuliya <m.i.m.o.z.a@mail.ru>
2022-11-30 20:00:59 +0400

    Задача 64.Вывод всех натуральных чисел в промежутке от N до 1 с помощью рекурсии.

---
+++ Seminar_9/HomeWork64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+int IntervalN1(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    Console.Write(n + ",");
+    return IntervalN1(n - 1);
+}
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int number = IntervalN1(n);
+Console.Write(number);
